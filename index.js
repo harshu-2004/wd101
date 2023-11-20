3ba2618105fc78229a67a3049d69a593f3444ad2
harshu-2004 <89955787+harshu-2004@users.noreply.github.com>
2023-11-20 22:20:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-<form id="registrationForm" onsubmit="return validateForm()">
-  <!-- Other form fields -->
+function validateForm() {
+    // Get form inputs
+    var nameInput = document.getElementById('name');
+    var emailInput = document.getElementById('email');
+    var passwordInput = document.getElementById('password');
+    var dobInput = document.getElementById('dob');
 
-  <label for="dob">Date of Birth:</label>
-  <input type="date" id="dob" name="dob" required>
+    // Validate Date of Birth
+    var dobValue = new Date(dobInput.value);
+    var currentDate = new Date();
+    var age = currentDate.getFullYear() - dobValue.getFullYear();
 
-  <!-- Other form fields -->
+    if (age < 18 || age > 55) {
+        alert('Please enter a valid date of birth between ages 18 and 55.');
+        return false; // Prevent form submission
+    }
 
-  <button type="submit">Submit</button>
-</form>
+    // You can add additional validations for name, email, and password as needed
+
+    // If all validations pass, continue with form submission
+    return true;
+}
